19dd9b4cddc3caffd614fdd5e48ba24fc709862e
HotMonkeyWings <devsony52@gmail.com>
2021-09-15 11:07:19 +0530

    Stuf

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+function invertColor(hex) {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    // invert color components
+    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+        g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+        b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    // pad each with zeros and return
+    return '#' + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+}
+
+
 function showTime() {
 
     var date = new Date()
@@ -14,15 +40,28 @@ function showTime() {
     m = (m < 10) ? "0" + m : m
     s = (s < 10) ? "0" + s : s
 
+
     var hex = h + m + s
+    var rgb = invertColor(hex)
 
     document.body.style.backgroundColor = "#" + hex
+
+
+
 
     var time = "#" + h + ":" + m + ":" + s + " "
     document.getElementById("MyClockDisplay").innerText = time
     document.getElementById("MyClockDisplay").textContent = time
+    document.getElementById("MyClockDisplay").style.color = rgb.toString()
 
     setTimeout(showTime, 1000)
+
 }
+
+
+
+
+
+
 
 showTime()
